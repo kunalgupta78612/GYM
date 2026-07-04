608403e2205f365744383dcd1b0689e99243b90f
kunal gupta <gkunal2021g@gmail.com>
2026-07-04 12:22:32 +0530

    update dotenv file

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import * as dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -13,8 +14,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended:true}));
-
-dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 // user defined package
